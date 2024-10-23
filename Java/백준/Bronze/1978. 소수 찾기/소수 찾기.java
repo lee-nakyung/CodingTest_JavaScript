48a552ba1d76d46b0770c8ab83b01ad2187ef75a
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2024-10-23 16:20:19 +0900

    [Bronze II] Title: 소수 찾기, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

--- Java/백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ Java/백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -12,23 +12,21 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int array[]=new int[N];
         int count = 0;
         
         
         for(int i=0; i<N; i++) {
-        	array[i] = Integer.parseInt(st.nextToken());
+        	int a = Integer.parseInt(st.nextToken());
         	boolean isPrime = true;
         	
-        	if(array[i]==1) {
+        	if(a==1) {
         		isPrime = false;
         	}
         	
         	else {
-        		for(int j=2; j*j<=array[i]; j++) {
-        			if(array[i]%j==0) {
+        		for(int j=2; j*j<=a; j++) {
+        			if(a%j==0) {
         				isPrime = false;
-        				break;
         			}
         		}
         	}
@@ -36,8 +34,6 @@ public class Main {
         	if(isPrime) {
         		count++;
         	}
-        	
-        
         }
         
         System.out.println(count);
