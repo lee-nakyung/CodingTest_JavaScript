55539194546153541625602cdc09ad4fcf2eade8
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2024-10-26 02:36:03 +0900

    [D1] Title: 신문 헤드라인, Time: 102 ms, Memory: 18,588 KB -BaekjoonHub

---
+++ Java/SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+       
+        String s = br.readLine();
+        
+       s= s.toUpperCase();
+       
+       bw.write(s);
+        
+        
+        bw.flush();
+    }
+    
+}
+
+
+
+
