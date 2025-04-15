8062090fd12d17d875a44ea9ddf8fe110df26f35
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-04-15 22:06:52 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ JavaScript/프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,9 +1,16 @@
 function solution(sides) {
+    var answer = 0;
     
-    sides.sort((a,b)=>a-b);
+    sides.sort(function(a,b){
+        return a-b;
+    }) //오름차순 정렬 2,3,6
+    
     if(sides[2]<sides[0]+sides[1]){
-        return 1;
+        answer = 1;
+    }else {
+        answer = 2;
     }
-    else return 2;
-
+    
+    
+    return answer;
 }
