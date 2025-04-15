56fc2eb46306885bcaa2743ca21528b0fe896984
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-04-15 17:50:04 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120829. 각도기/각도기.js
+++ JavaScript/프로그래머스/0/120829. 각도기/각도기.js
@@ -1,17 +1,5 @@
 function solution(angle) {
     var answer = 0;
     
-    if(0 < angle && angle <90){
-        answer = 1;
-    }
-    else if(angle === 90){
-        answer = 2;
-    }
-    else if(90<angle && angle<180){
-        answer = 3;
-    }
-    else {
-        answer = 4;
-    }
-    return answer;
+    return angle < 90 ? 1 : angle == 90 ? 2 : angle <180 ? 3 : 4
 }
