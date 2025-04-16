1831acb596841f89d0611714d4733afef976e38c
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-04-16 17:19:55 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ JavaScript/프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -1,3 +1,6 @@
 function solution(str1, str2) {
-    return str1.includes(str2) ? 1: 2;
+    
+    
+    //문자열 str1 안에 str2가 포함돼 있는지 확인하는 문제이므로, includes() 하나로 해결 가능.
+    return str1.includes(str2) ? 1 : 2;
 }
