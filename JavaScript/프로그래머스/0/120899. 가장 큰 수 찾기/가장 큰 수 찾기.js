dee3b8e21bee9b2df9ea79b721e4c2f0c0e8c8e8
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-04-20 21:31:18 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ JavaScript/프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,3 +1,15 @@
 function solution(array) {
-    return [Math.max(...array),array.indexOf(Math.max(...array))];
+    var answer = [];
+    
+    //가장 큰수는 Math.max로 구함
+    var max = Math.max(...array);
+    var index = 0;
+    for(let i=0; i<array.length; i++){
+        if(array[i]===max){
+            index = i;
+        }
+    }
+    
+    answer = [max,index];
+    return answer;
 }
