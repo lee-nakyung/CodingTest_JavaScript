294d01e6a780291483db2584039d0b710707cf2f
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-07-02 13:53:31 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.54 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ JavaScript/프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.55 ms
+메모리: 33.6 MB, 시간: 0.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 30일 15:18:57
+2025년 07월 02일 13:53:28
 
 ### 문제 설명
 
--- JavaScript/프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ JavaScript/프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,10 +1,13 @@
 function solution(numbers) {
     var answer = [];
+    var sum=0;
     
     for(let i=0; i<numbers.length; i++){
-        for(let j=0; j<i; j++){
-            answer.push(numbers[i]+numbers[j]);
+        for(let j=i+1; j<numbers.length; j++){
+            let sum = numbers[i]+numbers[j];
+            answer.push(sum);
         }
     }
+    
     return [...new Set(answer)].sort((a,b)=>a-b);
 }
