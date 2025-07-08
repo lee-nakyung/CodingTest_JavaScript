5df8eff7beec4adb40ab87513cb8ce6b0d2487ff
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-07-08 17:39:42 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.73 ms, Memory: 38.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ JavaScript/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,25 +1,16 @@
 function solution(arr1, arr2) {
-    var answer = [[]];
+    var answer = [];
     
-    const r1 = arr1.length;  //예시 1번 같은 경우는 2
-    const c1 = arr1[0].length; //예시 1번 같은 경우는 1
-    const r2 = arr2.length; //1
-    const c2 = arr2[0].length; // 2
-    
-    //2)결과를 저장할 2차원 배열 초기화
-    const ret=[];
-    for(let i=0; i<r1; i++){
-        ret.push(Array(c2).fill(0));
-    }
-    
-    //3)arr1과 arr2의 행렬을 곱하기 위한 반복문이다. 
-    for(let i=0; i<r1; i++){
-        for(let j=0; j<c2; j++){
-            for(let k=0; k<c1; k++){
-                ret[i][j]+=arr1[i][k]*arr2[k][j];
+    for(let i=0; i<arr1.length; i++){ // 행의 수
+        let row = [];
+        for(let j=0; j<arr2[0].length; j++){ // 열의 수
+            let sum = 0;
+            for(let k=0; k<arr2.length; k++){
+                sum+=arr1[i][k] * arr2[k][j];
             }
+            row.push(sum);
         }
+        answer.push(row);
     }
-    
-    return ret;
+    return answer;
 }
