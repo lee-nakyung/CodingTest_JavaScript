70fbbaeb1da33c5e0687d3982f732c4b1250f07a
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-07-17 23:39:10 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ JavaScript/프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,8 +1,7 @@
 function solution(numbers) {
     var answer = 0;
-    // 오름차순으로 정리를 하고, 맨 뒤에 요소랑 그 앞의 요소를 곱하면 되지 않나?
     
     numbers.sort((a,b)=>a-b);
-    answer = numbers[numbers.length-1] * numbers[numbers.length-2];
+    answer = numbers[numbers.length-1]*numbers[numbers.length-2];
     return answer;
 }
