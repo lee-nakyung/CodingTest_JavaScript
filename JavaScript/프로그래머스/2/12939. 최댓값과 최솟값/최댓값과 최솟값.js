e4eb57ee4ea7cc102b33d16df7e5973e5480c181
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-01 23:26:12 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ JavaScript/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -3,8 +3,8 @@ function solution(s) {
     
     // 최댓값 최솟값 구하기 위해서는 문자열에서 배열로 바뀌어야한다.
     // 문자열 -> 문자열 배열 -> 숫자로 된 배열
-    var numbers = s.split(" ").map(Number);
-    console.log(numbers);
+    var numbers = s.split(" ");
+    // 문자열이 들어오면 자동으로 숫자로 바꿔준다. 
     var max = Math.max(...numbers);
     var min = Math.min(...numbers);
     //최솟값 먼저 그다음 최댓값이 들어가야한다. 
