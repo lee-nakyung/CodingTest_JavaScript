23409a6047dec2501475a495fa116cb30cbfd528
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-03 16:46:31 +0900

    [level 2] Title: 숫자의 표현, Time: 2.32 ms, Memory: 36.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ JavaScript/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -7,9 +7,10 @@
 
 function solution(n) {
     var count = 0;
+
     
     for(let i=1; i<=n; i++){
-            let sum = 0;
+        let sum =0;
         for(let j=i; j<=n; j++){
             sum+=j;
             if(sum===n){
@@ -19,6 +20,7 @@ function solution(n) {
                 break;
             }
         }
+
     }
             return count;
 }
