9dea5fc979befa1601a06b511fc58c344b05f825
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-07 14:46:29 +0900

    [level 2] Title: 짝지어 제거하기, Time: 44.06 ms, Memory: 58.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12973. 짝지어 제거하기/README.md
+++ JavaScript/프로그래머스/2/12973. 짝지어 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 짝지어 제거하기 - 12973 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973) 
 
 ### 성능 요약
 
-메모리: 57.9 MB, 시간: 70.79 ms
+메모리: 58.2 MB, 시간: 44.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 10일 18:35:36
+2025년 08월 07일 14:46:24
 
 ### 문제 설명
 
--- JavaScript/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ JavaScript/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,16 +1,17 @@
 function solution(s)
 {
-    
     const stack = [];
+    // 일단 기본적으로는 스택에 하나씩 푸쉬를 한다. 
+    // 그런데 내 앞에 있는 문자열이 지금 내 문자랑 같으면 pop을 한다.
+    // 나중에 길이가 0이면 1을 반환하고, 그렇지 않으면 0을 반환한다.
     
     for(const c of s){
         if(stack.length > 0 && stack[stack.length-1] === c){
             stack.pop();
-        }
-        else {
+        }else {
             stack.push(c);
         }
     }
     
-    return stack.length===0 ? 1: 0;
+    return stack.length === 0 ? 1 : 0;
 }
