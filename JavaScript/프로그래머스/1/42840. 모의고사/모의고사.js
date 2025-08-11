e87a4424935815e42f500fedebd653828e69d81b
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-11 12:40:28 +0900

    [level 1] Title: 모의고사, Time: 29.13 ms, Memory: 36.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/42840. 모의고사/모의고사.js
+++ JavaScript/프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,15 +1,22 @@
 function solution(answers) {
+// 1. 일단 사람들별로 찍는 패턴을 선언
+// 2. 수포자들의 점수를 저장할 배열을 선언 
+// 3. answer.length와 patterns.length를 비교해서 만약 정답값이랑 같으면 scores에 +1
+// 4. 가장 높은 점수 저장 
+// 5. 가장 높은 점수를 받은 수포자들의 번호를 찾아서 배열에 담는다. 
+    
     const patterns = [
         [1,2,3,4,5],
         [2,1,2,3,2,4,2,5],
         [3,3,1,1,2,2,4,4,5,5],
-    ];
+    ]
     
-    const scores = [0,0,0];
+    const scores=[0,0,0];
     
-    for(const[i, answer] of answers.entries()){
-        for(const[j, pattern] of patterns.entries()){
-            if( answer === pattern[i%pattern.length]){
+    for(let i=0; i<answers.length; i++){
+        for(let j=0; j<patterns.length; j++){
+            const pick = patterns[j][i%patterns[j].length];
+            if(answers[i]===pick){
                 scores[j]+=1;
             }
         }
