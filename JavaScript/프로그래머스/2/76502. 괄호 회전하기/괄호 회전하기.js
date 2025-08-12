4341e0e12d9ee437bff8e98ab3ede7a0e805ce35
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-12 19:37:49 +0900

    [level 2] Title: 괄호 회전하기, Time: 11.87 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -0,0 +1,40 @@
+function solution(s) {
+  let answer = 0;
+  const n = s.length;
+
+  for (let i = 0; i < n; i++) {
+    const stack = [];
+    let isCorrect = true;
+
+    for (let j = 0; j < n; j++) {
+      const c = s[(i + j) % n];
+
+      if (c === "[" || c === "(" || c === "{") {
+        stack.push(c);
+      } else {
+        if (stack.length === 0) {
+          isCorrect = false; 
+          break;
+        }
+        const top = stack[stack.length - 1];
+        if (
+          (c === "]" && top === "[") ||
+          (c === ")" && top === "(") ||
+          (c === "}" && top === "{")
+        ) {
+          stack.pop();
+        } else {
+          isCorrect = false; 
+          break;
+        }
+      }
+    }
+
+  
+    if (isCorrect && stack.length === 0) {
+      answer++;
+    }
+  }
+
+  return answer;
+}
