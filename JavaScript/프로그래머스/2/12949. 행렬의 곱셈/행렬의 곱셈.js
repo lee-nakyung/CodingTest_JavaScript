90a51fb036102548ad51b3393de3ae5b809d0359
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-12 15:36:21 +0900

    [level 2] Title: 행렬의 곱셈, Time: 9.86 ms, Memory: 38.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ JavaScript/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,16 +1,22 @@
-function solution(arr1, arr2) {
-    var answer = [];
-    
-    for(let i=0; i<arr1.length; i++){ // 행의 수
-        let row = [];
-        for(let j=0; j<arr2[0].length; j++){ // 열의 수
-            let sum = 0;
-            for(let k=0; k<arr2.length; k++){
-                sum+=arr1[i][k] * arr2[k][j];
-            }
-            row.push(sum);
-        }
-        answer.push(row);
+function solution(arr1, arr2){
+	const arr1y = arr1.length; // 결과의 행
+	const arr2x = arr2[0].length;  // 결과의 열 
+	
+    // 결과 배열을 0으로 채워서 반환한다.
+    const result=[];
+	for(let i=0; i<arr1y; i++){
+        result.push(new Array(arr2x).fill(0));
     }
-    return answer;
+    
+    for(let i=0; i<arr1y; i++){
+        for(let j=0; j<arr2x; j++){
+            for(let k=0; k<arr1[0].length; k++){
+                result[i][j] += arr1[i][k] * arr2[k][j];
+            }
+        }
+    }
+    
+	
+	return result;
+
 }
