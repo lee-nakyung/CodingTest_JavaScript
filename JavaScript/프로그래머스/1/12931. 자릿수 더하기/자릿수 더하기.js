968b4a3750b540ca4c86d92ebce2b27e8fe5f83f
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-19 21:47:34 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ JavaScript/프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,10 +1,12 @@
+// 숫자를 배열로 바꿔서 i를 순회해서 더하기
 function solution(n)
 {
     var answer = 0;
-    
-    var arr = n.toString(); //숫자를 -> 문자열로 변환 , 문자열은 + 하면 그대로 붙으니까
-    for(let i=0; i<arr.length; i++){
-        answer+=Number(arr[i]);
+
+    var array = n.toString().split("");
+    for(let i=0; i<array.length; i++){
+        answer+=Number(array[i]);
     }
+
     return answer;
 }
