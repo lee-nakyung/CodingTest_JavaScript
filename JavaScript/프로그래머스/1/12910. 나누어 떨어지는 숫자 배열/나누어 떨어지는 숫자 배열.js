2f9a191af2d42c264e8c5270848d4a24846a0563
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-20 21:23:38 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 2.56 ms, Memory: 38.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
+++ JavaScript/프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -1,19 +1,12 @@
+// arr을 순회해서 만약에 divisor로 나눠떨어지면 , 그 값은 이제 answer배열에 푸쉬한다.
+
 function solution(arr, divisor) {
     var answer = [];
     
-    
-    //arr만큼 쭉 돌아서 만약 나눠떨어지면 answer 배열에 푸쉬
     for(let i=0; i<arr.length; i++){
         if(arr[i]%divisor ===0){
             answer.push(arr[i]);
         }
     }
-    
-    //for문을 돌았는데 answer의 배열 길이가 0 이면 -1 푸쉬
-    if(answer.length===0){
-        answer.push(-1);
-    }
-    
-    //오름차순으로 정렬해서 리턴
-    return answer.sort((a,b)=>a-b);
+    return answer.length === 0 ? [-1] : answer.sort((a,b)=>a-b)
 }
