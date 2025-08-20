faf9053f110d714cae6f9799c99e04d2e02a979b
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-20 22:10:51 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.15 ms, Memory: 32.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ JavaScript/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,14 +1,25 @@
 function solution(num) {
-    var answer = 0;
+var answer = 0;
     
-   for(let i=0; i<500; i++){
-       if( num ===1) return i; 
+    if(num===1){
+        answer = 0;
+    }
+    
+    while(num!==1){
+        if(num%2===0){
+            num=num/2;
+            answer++;
+        }
+        else if (num%2===1){
+            num=num*3+1;
+            answer++;
+        }
        
-       if(num%2===0) {
-           num = num/2;
-       } else  {
-           num = num*3+1;
-       }
-   }
-    return -1;
+    }
+    
+    if(answer >=500){
+        answer = -1;
+    }
+    
+    return answer;
 }
