5d1c422f86235752f50cfaf1ae5bd2a9e9e07e23
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-20 20:29:54 +0900

    [level 1] Title: 음양 더하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ JavaScript/프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,14 +1,14 @@
+//signs를 배열로 돌려서 true이면 absolutes[i]를 그대로 반환하고 false이면-를 넣도록
 function solution(absolutes, signs) {
     var answer = 0;
     
-    // *-1을 하면 음수로 변함 
-    
-    for(let i=0; i<absolutes.length; i++){
-        if(signs[i]==true){
-            answer+=absolutes[i];
-        } else if (signs[i]==false){
-            answer+=absolutes[i]*-1;
+    for(let i=0; i<signs.length; i++){
+        if(signs[i]===false){
+            absolutes[i]= absolutes[i]*-1;
         }
+        answer+=absolutes[i];
+        
     }
+    
     return answer;
 }
