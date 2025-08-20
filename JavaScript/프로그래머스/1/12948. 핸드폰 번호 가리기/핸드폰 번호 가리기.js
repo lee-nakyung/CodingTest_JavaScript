12038f900a6472f30b53025736385516924d0141
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-20 22:26:23 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ JavaScript/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,11 +1,3 @@
 function solution(phone_number) {
-    var answer = '';
-    
-    for(let i=0; i<phone_number.length; i++){
-        if(i<phone_number.length-4){
-            answer+="*";
-        }
-    }
-      answer += phone_number.slice(-4);
-    return answer;
+    return "*".repeat(phone_number.length-4)+phone_number.slice(-4);
 }
