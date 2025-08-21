a7dd9ceabeb391bf25ee25fbda0667aab5215243
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-22 01:00:17 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 32.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ JavaScript/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,10 +1,6 @@
-// 문자열 -> 배열 -> 숫자 배열
-//최솟값과 최대값만 남겨야하므로, 인덱스 0 / 그리고 s.length-1
-// 만약 s의 0 과 1이 같으면 그냥 그 s의 값을 리턴한다.
+// 문자열이니까 배열로 반환을 해야합니다. split 메소드 사용
 
 function solution(s) {
-    var answer = s.split(" ");
-
-    
+    var answer = s.split(" "); // ["1","2","3","4"]
     return Math.min(...answer)+" "+Math.max(...answer);
 }
