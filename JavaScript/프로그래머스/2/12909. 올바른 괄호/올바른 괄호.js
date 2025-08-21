0150ac1a793929f07dccba3e92c88febe06dc9b9
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-22 01:05:29 +0900

    [level 2] Title: 올바른 괄호, Time: 27.39 ms, Memory: 36.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ JavaScript/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,21 +1,19 @@
-// count 변수를 선언해서, 
-// (를 만나면 +1 )를 만나면 -1 를 한다.
-// 근데 만약 )를 만났는데 그 때의 count 값이 0이면, false를 반환한다. 
-// count값이 최종 0 이면 , true를 반환하고 그렇지 않으면 false를 반환한다.
-
 function solution(s){
     var count = 0;
-
+    
     for(let i=0; i<s.length; i++){
-        if(s[i]=="("){
+        if(s[i]==="("){
             count++;
-        } else if (s[i]==")"){
+        }
+        else if (s[i]===")"){
             if(count===0){
                 return false;
             }
-            count--;
+            else {
+                count--;
+            }
         }
     }
-    
-    return count ===0 ? true : false;
+
+    return count === 0 
 }
