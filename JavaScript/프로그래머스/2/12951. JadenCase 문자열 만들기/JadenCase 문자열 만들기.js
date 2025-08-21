8626092d89f60788613a47c4d83dc72d5d63bf9a
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-21 20:07:48 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ JavaScript/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,12 +1,19 @@
+// 첫 문자는 대문자, 나머지는 소문자가 기본
+// 첫 문자가 알파벳이 아닐 때에는 그 다음 문자는 소문자. 
+// 첫 문자를 판별하려면? 그 앞이 공백이여야함. 
+// 따라서 공백이면 ? 그 다음 s[i]가 소문자. 
+
 function solution(s) {
-    var answer = [];
+    var answer = '';
     
     for(let i=0; i<s.length; i++){
-        if(i===0 || s[i-1]===" "){
-            answer.push(s[i].toUpperCase());
-        }else {
-            answer.push(s[i].toLowerCase());
-        }
+       if(s[i-1]===" "){
+           answer+=s[i].toUpperCase();
+       } else if (i===0){
+           answer+=s[i].toUpperCase();
+       } else {
+           answer+=s[i].toLowerCase();
+       }
     }
-    return answer.join("");
+    return answer;
 }
