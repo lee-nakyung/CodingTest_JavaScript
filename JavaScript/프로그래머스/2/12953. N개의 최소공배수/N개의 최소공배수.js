f73eb658ec480be0ea150c1e014f04b8409e26fc
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-22 00:35:54 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ JavaScript/프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,17 +1,22 @@
-// 최대공약수
+//최대공약수 구하는 법 : gcd(b,a%b);
+
 function gcd(a,b){
-    return b===0 ? a : gcd(b,a%b);
+    if(b===0){
+        return a;
+    }
+    return gcd(b,a%b);
 }
 
-// 최소공배수
+//최소공배수
 function lcm(a,b){
     return (a*b)/gcd(a,b);
 }
 
 function solution(arr) {
-    var result = arr[0];
+    let answer = arr[0];
+    
     for(let i=1; i<arr.length; i++){
-        result = lcm(result,arr[i]);
+        answer = lcm(answer,arr[i]);
     }
-    return result;
+    return answer;
 }
