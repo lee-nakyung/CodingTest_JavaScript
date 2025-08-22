b5e15724b84875249710cf69f6cd93d0caefdcee
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-22 12:41:45 +0900

    [level 2] Title: 짝지어 제거하기, Time: 51.20 ms, Memory: 51 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ JavaScript/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,7 +1,11 @@
+// 스택을 이용하자. 
+// 일단 모든 것을 다 푸쉬를 하는데, 스택에 들어간 마지막 것과 지금 넣으려는 것이 같으면 Pop
+// 그래서 스택의 길이가 0이면 1, 아니면 0을 리턴한다.
 function solution(s)
 {
-    var stack = [];
-    for(let i=0; i<s.length; i++){ // 0부터 5까지 
+    var stack=[];
+
+    for(let i=0; i<s.length; i++){
         if(stack[stack.length-1]===s[i]){
             stack.pop();
         }
@@ -10,5 +14,5 @@ function solution(s)
         }
     }
     
-    return stack.length===0 ? 1 : 0;
+    return stack.length===0 ? 1: 0;
 }
