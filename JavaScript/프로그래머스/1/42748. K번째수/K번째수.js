22efda287424521914bf6a0764f7f18aa08b94f7
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-22 22:16:40 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/42748. K번째수/K번째수.js
+++ JavaScript/프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,12 +1,14 @@
+// 1,5,2,6,3,7,4 가 있고 2번째부터 5번째까지면 인덱스 1부터 4까지 자른다.
+// 5,2,6,3을 정렬한다. => 2,3,5,6
+// 3이니까 인덱스 2는 5 
 function solution(array, commands) {
     var answer = [];
     
-    //commands의 전체를 순회를 해야한다.
-    
     for(let i=0; i<commands.length; i++){
-        const [a,b,c] = commands[i]; // 2 5 3
-        const sliced = array.slice(a-1,b).sort((a,b)=>a-b);
-        answer.push(sliced[c-1]);
+                let [start,end,k]=commands[i];
+       
+        let result = array.slice(start-1,end).sort((a,b)=>a-b);
+        answer.push(result[k-1]);
     }
     return answer;
 }
