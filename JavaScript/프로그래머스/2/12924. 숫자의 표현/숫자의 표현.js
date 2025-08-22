772383c091edcc81c89c0faa224e3c528c419911
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-22 12:36:18 +0900

    [level 2] Title: 숫자의 표현, Time: 2.77 ms, Memory: 36.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ JavaScript/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,21 +1,17 @@
-// 1. i는 1부터 n까지 순회한다.
-// 2. while문으로 sum값을 구해서 누적한다. j를 인덱스로 하여 sum값을 구하면 ++ 해준다.
-// 3. sum과 n이 같아지면 count가 증가되고 break문으로 탈출한다.
-// 4. count를 리턴한다.
+// i가 1부터 n까지 쭉 순회하도록 한다.
+//
 
 function solution(n) {
     var answer = 0;
-    let count = 0;
     
     for(let i=1; i<=n; i++){
-        let j = i;
-            let sum = 0;
-        
-        while(sum<n){
+        let j=i;
+        let sum = 0;
+        while(sum<=n){
             sum+=j;
             j++;
-            if(sum==n){
-                count++;
+            if(sum===n){
+                answer++;
                 break;
             }
             if(sum+j>n){
@@ -23,5 +19,5 @@ function solution(n) {
             }
         }
     }
-    return count;
+    return answer;
 }
