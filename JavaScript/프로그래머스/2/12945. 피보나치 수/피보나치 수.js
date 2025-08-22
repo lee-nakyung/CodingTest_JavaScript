bc49424477d94b1104a147a3e7f15a197aca1fd6
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-22 12:51:28 +0900

    [level 2] Title: 피보나치 수, Time: 29.53 ms, Memory: 37.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ JavaScript/프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,12 +1,12 @@
+// 피보나치 dp 활용
 function solution(n) {
-    let t1 = 0;
-    let t2 = 1;
-    let answer = 0;
+    var answer = 0;
+    var dp = new Array(n+1).fill(0); // 배열 선언
+    dp[0]=0;
+    dp[1]=1;
     
     for(let i=2; i<=n; i++){
-        answer = (t1 + t2) %1234567;
-        t1 = t2;
-        t2 = answer;
+        dp[i] = (dp[i-1] + dp[i-2])%1234567;
     }
-    return answer;
+    return dp[n];
 }
