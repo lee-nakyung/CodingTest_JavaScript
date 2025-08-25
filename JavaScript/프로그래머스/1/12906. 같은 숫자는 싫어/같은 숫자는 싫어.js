67a7f7a5f31fd710f17acfdd7cb546979a22b5b0
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-25 13:11:54 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.90 ms, Memory: 82.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ JavaScript/프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,9 +1,20 @@
+// 스택을 이용한다
+// 스택에 넣고. 그 stack[stack.length-1] 이랑 지금 넣으려는 것이 같으면 pop을 한다.
+// 그리고 리턴한다.
+// 만약 지금 스택에 
+
 function solution(arr)
 {
-    var answer = [];
-    for (let i = 0; i < arr.length; i++) {
-      if(arr[i] !== arr[i+1]) answer.push(arr[i])
+    var stack=[];
+    
+    for(let i=0; i<arr.length; i++){
+        if(stack[stack.length-1]===arr[i]){
+            continue;
+        }
+        else {
+            stack.push(arr[i]);
+        }
     }
-    return answer;
+    
+    return stack;
 }
-
