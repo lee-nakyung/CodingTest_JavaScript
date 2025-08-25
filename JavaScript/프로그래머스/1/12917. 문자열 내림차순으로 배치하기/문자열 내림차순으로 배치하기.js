a1504b21d8b101bdf891199027cd13e9c892fc89
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-25 16:07:59 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ JavaScript/프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,10 +1,10 @@
+// 문자열을 내림차순으로 배치하기.
+// 그러면 일단 문자열을 배열로 바꿔야한다.
+// s.split(" ")
+
+
 function solution(s) {
-    
-//     split을 하면 문자열을 배열로 바꿔줌 따라서, Zbcdefg -> ["Z","b","c","d","e","f","g"]
-//     숫자 정렬할 때만 (a,b)=>a-b
-// 얘는 문자이기 때문에 sort() 그리고 역순이니까 reverse()
-    // join() = 배열 -> 문자열로 다시 합치기 
-    var array = s.split("").sort().reverse().join("");
-    
-    return array;
+    var answer = '';
+    answer = s.split("").sort().reverse().join("");
+    return answer;
 }
