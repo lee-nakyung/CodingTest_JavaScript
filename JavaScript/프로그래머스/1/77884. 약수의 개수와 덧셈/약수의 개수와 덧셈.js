2b81c96814847b3e89f5717e4f21eec9ede3608f
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-25 15:40:10 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 442.95 ms, Memory: 35.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ JavaScript/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,27 +1,27 @@
+// 일단 약수의 갯수를 구하기
+// 만약 약수의 갯수가 짝수이면 answer에다가 더하고
+// 약수의 갯수가 홀수이면 answer에다가 빼기
+
 function solution(left, right) {
-    var sum = 0;
+   
+    var sum = 0; // return 해야 할 결과값
     
-    
-    //전체 순회
     for(let i=left; i<=right; i++){
-        
-        var count = 0; //약수의 갯수
-        
-        for(let j=1; j<=i; j++){
+         var answer = 0; // 약수의 갯수를 담을 변수 
+        for(let j=1; j<=i; j++){ 
             if(i%j===0){
-                count++;
+                answer++;
             }
-             }
             
-            if(count%2===0){
-                sum+=i;
-            }
-            else {
-                sum-=i;
-            }
-       
-    
+        }
         
+        if(answer%2===0){
+            sum+=i;
+        } else {
+            sum-=i;
+        }
     }
-     return sum;
+    
+    return sum;
+   
 }
