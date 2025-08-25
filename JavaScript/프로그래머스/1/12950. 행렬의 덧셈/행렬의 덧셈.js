722bff890f4c9cec25411c8f451e2d1cba593f03
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-25 17:44:09 +0900

    [level 1] Title: 행렬의 덧셈, Time: 9.11 ms, Memory: 67.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ JavaScript/프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,16 +1,15 @@
+
+
 function solution(arr1, arr2) {
-    var answer = [];
-    
-    
+    let answer=[];
+
     for(let i=0; i<arr1.length; i++){
-             var sum =[];
+        let row = [];
+        
         for(let j=0; j<arr1[i].length; j++){
-       
-            sum.push(arr1[i][j]+arr2[i][j]);
+            row.push(arr1[i][j]+arr2[i][j]);
         }
-         answer.push(sum);
- 
+        answer.push(row);
     }
     return answer;
- 
 }
