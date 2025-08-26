5d9205624e8afe846792b8475859aec8adf83498
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-26 13:58:38 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ JavaScript/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,8 +1,11 @@
-// N진법 구하는 법 => n.toString(3);
-// 
-
+// 숫자를 뒤집으려면 문자열로 바꾼 뒤에, reverse를 해야함
+//3진법으로 바꾸고, reverse하고, 10진법
+// n진법으로 바꿀때에는 toString()
+// 10진법으로 바꿀 때에는 이전 진법을 표기한다 parseInt(number,3);
 function solution(n) {
     var answer = 0;
-    answer = n.toString(3).split("").reverse().join("").toString(10);
-    return parseInt(answer,3);
+    
+    answer = parseInt(n.toString(3).split("").reverse().join(""),3);
+    
+    return answer;
 }
