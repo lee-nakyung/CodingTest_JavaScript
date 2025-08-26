73a2697dc5132bb128c029be4eabdff9a6d5df19
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-26 22:33:07 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 7.55 ms, Memory: 38.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ JavaScript/프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,4 +1,19 @@
+// 만약 문자열을 순회하는데 0 이 있으면 제거하고, 그 카운트를 센다.
+// 제거한 길이를 구한다.
+// 그 길이를 2진 변환한다.
+
+
 function solution(s) {
     var answer = [];
-    return answer;
+    var removed = 0;
+    var count = 0;
+    
+    while(s!=="1"){
+        const oneleng = [...s].filter(item=> item==="1").length;  //3
+        removed += s.length-oneleng; // 제거한 0 의 갯수 (s의 길이 - oneleng) == 2
+        s = oneleng.toString(2);
+        
+        count++;
+    }
+    return [count,removed];
 }
