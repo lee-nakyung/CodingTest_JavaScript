ce52bdb42ee13f46e2967bc94182aff03ccc5b4e
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-27 23:16:23 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.68 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,13 @@
+function solution(food) {
+    var answer = [];
+    let arr = []
+    for (let i = 1; i < food.length; i++){
+        arr.push(Math.floor(food[i] / 2))
+    }
+    for (let i = 0; i < arr.length; i++){
+        for (let j = 0; j < arr[i]; j++){
+            answer.push(i + 1)
+        }
+    }
+    return [...answer, 0].concat(answer.reverse()).join('');
+}
