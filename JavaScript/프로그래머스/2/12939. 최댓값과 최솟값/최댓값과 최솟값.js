67583886d75b6e7cb7929c25c4a1e1e0561e640f
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-08-31 22:41:39 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ JavaScript/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,6 +1,6 @@
-// 문자열이니까 배열로 반환을 해야합니다. split 메소드 사용
+// 문자열이니까 배열로 반환을 해야합니다. split 메소드
 
 function solution(s) {
-    var answer = s.split(" "); // ["1","2","3","4"]
+    var answer = s.split(" ");
     return Math.min(...answer)+" "+Math.max(...answer);
 }
